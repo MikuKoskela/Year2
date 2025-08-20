daf732b382c544d6f589a0e05de0f1fb361aa5a6
miku <miku.koskela@gmail.com>
2025-08-20 15:32:08 +0300

    re init

---
+++ app.js
@@ -0,0 +1,16 @@
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+
+//Register a listener
+emitter.on('messageLogged', function(arg){
+    console.log('Listener Called', arg );
+});
+
+//Raise an event
+emitter.emit('messageLogged', { id:1, url:'http://'});
+
+
+
+
+//Raise: logging (data: message )
